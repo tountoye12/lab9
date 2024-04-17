4d046b6a7d99d0cdad3d1e768380b58b5b7245c6
diallo <mamadouadama609@gmail.com>
2024-04-17 16:25:05 -0500

    adding the user and role models

--- src/main/java/com/diallo/lab7/model/User.java
+++ src/main/java/com/diallo/lab7/model/User.java
@@ -6,19 +6,34 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "user")
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    private String firstName;
-    private String lastName;
     private String email;
     private String password;
+    @OneToMany(mappedBy = "user")
+    private List<Role> roles = new ArrayList<>();
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+    public void removeRole(Role role) {
+        roles.remove(role);
+    }
+
+    public String [] getRoleNames(){
+        return roles.stream()
+                .map(Role::getRoleName).toArray(String[]::new);
+    }
+
+
 
 }
